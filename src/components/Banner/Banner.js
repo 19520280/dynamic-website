63204f00a4fb3a8d0804096102346df9a3725f5e
Thao <19520280@gm.uit.edu.vn>
2021-12-10 15:31:50 +0700

    Responsive

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -3,15 +3,15 @@ import "./Banner.css";
 import {
   Box,
   Button,
-  Collapse,
   Grow,
-  Paper,
   Slide,
-  Stack,
   Typography,
-  Zoom,
+  useMediaQuery,
+  useTheme,
 } from "@mui/material";
 import React, { useEffect, useState } from "react";
+
+import { ChevronRight } from "@mui/icons-material";
 
 const slide1 =
   require("../../assets/images/banners/complex_slide01.jpg").default;
@@ -19,13 +19,17 @@ const slide1 =
 const slide2 =
   require("../../assets/images/banners/complex_slide02.jpg").default;
 const Banner = () => {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+
+  /* #region  use for transition */
   const [checked, setchecked] = useState(false);
   useEffect(() => {
     setchecked(true);
   });
-
   const containerRef = React.useRef(null);
-  console.log(checked);
+  /* #endregion */
+
   return (
     <div className="fullwidthbanner-container">
       <div className="fullwidthbanner">
@@ -50,7 +54,7 @@ const Banner = () => {
               >
                 <Typography
                   fontWeight="light"
-                  fontSize="calc(0.8rem + 1vw)"
+                  fontSize="calc(0.04rem + 2vw)"
                   color="secondary"
                 >
                   Chào tháng 12
@@ -62,7 +66,7 @@ const Banner = () => {
                 style={{ transitionDelay: checked ? "500ms" : "0ms" }}
               >
                 <Typography
-                  fontSize="calc(2rem + 1vw)"
+                  fontSize="calc(1.4rem + 2vw)"
                   fontWeight="bold"
                   color="secondary"
                 >
@@ -77,7 +81,7 @@ const Banner = () => {
                 {...{ timeout: 1000 }}
               >
                 <Typography
-                  fontSize="calc(0.8rem + 1vw)"
+                  fontSize="calc(0.05rem + 2vw)"
                   fontWeight="light"
                   color="secondary"
                 >
@@ -97,7 +101,7 @@ const Banner = () => {
                   fontSize="calc(0.8rem + 0.05vw)"
                   fontWeight="light"
                   color="secondary"
-                  margin="16px 0px 16px 0px"
+                  margin="16px 0px 0px 0px"
                 >
                   <>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit,
@@ -105,8 +109,18 @@ const Banner = () => {
                     magna aliqua.
                   </>
                 </Typography>
-                <Button variant="contained">Khám phá</Button>
-                <Button variant="outlined" sx={{m:2}}>Mua ngay</Button>
+                {isMobile ? (
+                  <Button variant="text" endIcon={<ChevronRight />} sx={{padding:"0px", fontSize:"12px"}}>
+                    Khám phá
+                  </Button>
+                ) : (
+                  <>
+                    <Button variant="contained" >Khám phá</Button>
+                    <Button variant="outlined" sx={{ m:1}}>
+                      Mua ngay
+                    </Button>
+                  </>
+                )}
               </Box>
             </Slide>
           </Box>
