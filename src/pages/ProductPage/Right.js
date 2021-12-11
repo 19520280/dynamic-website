6588aefd377a45104e8b89c3af2f0ed5745a999a
yccyennhi <ycc.yennhi@gmail.com>
2021-12-11 20:17:18 +0700

    update resỉze UI

--- src/pages/ProductPage/Right.js
+++ src/pages/ProductPage/Right.js
@@ -1,6 +1,7 @@
 import React from "react";
+import "./style.css";
 import { makeStyles } from "@material-ui/core";
-import { useLocation } from "react-router-dom";
+import { useLocation, useHistory } from "react-router-dom";
 import Carousel from "react-material-ui-carousel";
 import {
   Typography,
@@ -13,6 +14,7 @@ import {
   Container,
   Tab,
   Tabs,
+  Button,
 } from "@mui/material";
 import TabContext from "@material-ui/lab/TabContext";
 import TabList from "@material-ui/lab/TabList";
@@ -33,15 +35,18 @@ const useStyle = makeStyles({
     lineHeight: "48px",
     color: "#303557",
     backgroundColor: "#FCFCFC",
+    // maxWidth: "958px",
   },
   image: {
-    height: "324px",
-    width: "958px",
+    maxWidth: "100%",
+    height: "auto",
+    // width: "auto\9";
   },
 });
 
 const Right = () => {
   const classes = useStyle();
+  const history = useHistory();
   const location = useLocation();
   const [value, setValue] = React.useState("1");
   const handleChange = (event, newValue) => {
@@ -56,12 +61,28 @@ const Right = () => {
   ];
 
   return (
-    <Container style={{ width: "958px" }}>
+    <Container style={{ maxWidth: "100%" }}>
       <Typography component="div" className={classes.header}>
         Áo
       </Typography>
-      <Carousel autoPlay swipe animation="slide">
-        <img className={classes.image} src={slide2} />
+      <Carousel autoPlay swipe animation="slide" Button>
+        {/* <div className="container">
+          <img src={slide2} alt="" className={classes.image} />
+          <p className="title">card title</p>
+          <div className="overlay"></div>
+          <div className="button">
+            <a href="#"> BUTTON </a>
+          </div>
+        </div> */}
+
+        <div className="container">
+          <img src={slide2} alt="Avatar" className={classes.image} />
+          <div className="middle">
+            <Button variant="contained" className="text" onClick={()=>{history.push("/")}}>
+              XEM THÊM
+            </Button>
+          </div>
+        </div>
         <img className={classes.image} src={slide2} />
         <img className={classes.image} src={slide2} />
       </Carousel>
@@ -76,14 +97,13 @@ const Right = () => {
               indicatorColor="primary"
               aria-label="primary tabs example"
             >
-            <Tab value="1" label="PHỔ BIẾN" />
+              <Tab value="1" label="PHỔ BIẾN" />
               {menuItems.map((item) => (
                 <Tab value={item.value} label={item.text} />
               ))}
             </Tabs>
           </Box>
           <TabPanel value="1">Item PHỔ BIẾN</TabPanel>
-
           {menuItems.map((item) => (
             <TabPanel value={item.value}>Item {item.text}</TabPanel>
           ))}
