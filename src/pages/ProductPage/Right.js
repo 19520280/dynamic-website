b1bfa377e8ca8a20c2461ab81124277a930b3478
yccyennhi <ycc.yennhi@gmail.com>
2021-12-11 18:00:57 +0700

    update UI

--- src/pages/ProductPage/Right.js
+++ src/pages/ProductPage/Right.js
@@ -11,13 +11,17 @@ import {
   useMediaQuery,
   useTheme,
   Container,
+  Tab,
+  Tabs,
 } from "@mui/material";
+import TabContext from "@material-ui/lab/TabContext";
+import TabList from "@material-ui/lab/TabList";
+import TabPanel from "@material-ui/lab/TabPanel";
 
 const slide1 =
   require("../../assets/images/banners/complex_slide01.jpg").default;
 
-const slide2 =
-  require("../../assets/images/banners/SaleBanner.png").default;
+const slide2 = require("../../assets/images/banners/SaleBanner.png").default;
 
 const useStyle = makeStyles({
   header: {
@@ -32,25 +36,59 @@ const useStyle = makeStyles({
   },
   image: {
     height: "324px",
-    width: "958px"
-  }
+    width: "958px",
+  },
 });
 
 const Right = () => {
   const classes = useStyle();
   const location = useLocation();
-  console.log(location.pathname);
+  const [value, setValue] = React.useState("1");
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const menuItems = [
+    { value: 2, text: "BÁN CHẠY" },
+    { value: 3, text: "MỚI NHẤT" },
+    { value: 4, text: "GIÁ CAO" },
+    { value: 5, text: "GIÁ THẤP" },
+  ];
 
   return (
-    <Container style={{width:"958px"}}>
+    <Container style={{ width: "958px" }}>
       <Typography component="div" className={classes.header}>
         Áo
       </Typography>
       <Carousel autoPlay swipe animation="slide">
-        <img className={classes.image} src={slide2} /> 
-        <img className={classes.image} src={slide2} /> 
-        <img className={classes.image} src={slide2} /> 
+        <img className={classes.image} src={slide2} />
+        <img className={classes.image} src={slide2} />
+        <img className={classes.image} src={slide2} />
       </Carousel>
+      <Box sx={{ width: "100%", typography: "body1" }}>
+        <TabContext indicatorColor="primary" value={value}>
+          <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+            <Tabs
+              value={value}
+              defaultValue={value}
+              onChange={handleChange}
+              textColor="primary"
+              indicatorColor="primary"
+              aria-label="primary tabs example"
+            >
+            <Tab value="1" label="PHỔ BIẾN" />
+              {menuItems.map((item) => (
+                <Tab value={item.value} label={item.text} />
+              ))}
+            </Tabs>
+          </Box>
+          <TabPanel value="1">Item PHỔ BIẾN</TabPanel>
+
+          {menuItems.map((item) => (
+            <TabPanel value={item.value}>Item {item.text}</TabPanel>
+          ))}
+        </TabContext>
+      </Box>
     </Container>
   );
 };
