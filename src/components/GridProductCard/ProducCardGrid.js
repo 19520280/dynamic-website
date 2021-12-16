43f9a68b4a7e88052a009320ab99c2a84792ebd0
Thao <19520280@gm.uit.edu.vn>
2021-12-16 23:35:43 +0700

    fix products data

--- src/components/GridProductCard/ProducCardGrid.js
+++ src/components/GridProductCard/ProducCardGrid.js
@@ -1,24 +1,42 @@
 import { Box, Grid } from "@mui/material";
 
-import ProductCard from './../ProductCard/ProductCard';
-import React from 'react'
+import ProductCard from "./../ProductCard/ProductCard";
+import React from "react";
+import { products } from "../../dataSources/Products";
 
-const ProducCardGrid = () => {
-    return (
-        <Box sx={{ flexGrow: 1 }}>
-        <Grid
-          container
-          spacing={{ xs: 1, md: 2 }}
-          columns={{ xs: 2, sm: 6, md: 8 }}
-        >
-          {Array.from(Array(8)).map((_, index) => (
-            <Grid item xs={1} sm={2} md={2} key={index}>
-              <ProductCard />
-            </Grid>
-          ))}
-        </Grid>
-      </Box>
-    )
-}
+const ProducCardGrid = ({ phanLoai, lengthGrid }) => {
+  React.useEffect(() => {
+    if (lengthGrid) {
+      products.length = lengthGrid;
+    }
+  });
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid
+        container
+        spacing={{ xs: 1, md: 2 }}
+        columns={{ xs: 2, sm: 6, md: 8 }}
+      >
+        {products.map((sanPham, index) => {
+          if (phanLoai) {
+            if (sanPham.phanLoai == phanLoai) {
+              return (
+                <Grid item xs={1} sm={2} md={2} key={index}>
+                  <ProductCard sanPham={sanPham} />
+                </Grid>
+              );
+            }
+          } else {
+            return (
+              <Grid item xs={1} sm={2} md={2} key={index}>
+                <ProductCard sanPham={sanPham} />
+              </Grid>
+            );
+          }
+        })}
+      </Grid>
+    </Box>
+  );
+};
 
-export default ProducCardGrid
+export default ProducCardGrid;
