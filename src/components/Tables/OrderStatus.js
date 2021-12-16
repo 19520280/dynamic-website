0f4d300ea91f450d0a8f8d10aec9ccd6294d8a3c
yccyennhi <ycc.yennhi@gmail.com>
2021-12-17 05:09:41 +0700

    Update OrderStatus.js

--- src/components/Tables/OrderStatus.js
+++ src/components/Tables/OrderStatus.js
@@ -3,11 +3,11 @@ import React from "react";
 import OrdersCaseTypography from "../Typographys/OrdersCaseTypography";
 import CheckCartLeftTable from "./CheckCartLeftTable";
 
-export default function OrderStatus({status}) {
+export default function OrderStatus({ status }) {
   return (
     <Grid sx={{ paddingTop: "12px" }}>
       <Grid item container justifyContent="flex-end">
-        <OrdersCaseTypography text={status?status:"TẤT CẢ"} />
+        <OrdersCaseTypography text={status ? status : "TẤT CẢ"} />
       </Grid>
       <CheckCartLeftTable />
     </Grid>
