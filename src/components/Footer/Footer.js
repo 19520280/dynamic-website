7756883e184ea28076913d9e14bf153e06902024
yccyennhi <ycc.yennhi@gmail.com>
2021-12-17 20:37:26 +0700

    fix font size filter

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,19 +1,39 @@
-import React from 'react'
-import AppBar from '@mui/material/AppBar';
-import Container from '@mui/material/Container';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
+import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import { Container, Grid, Slide, useMediaQuery, useTheme } from "@mui/material";
 
 export default function Footer() {
+  const theme = useTheme();
+
+  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
+  if (!isMobile) {
     return (
+      <>
         <AppBar position="static" color="primary">
-          <Container maxWidth="md">
+          <Container>
             <Toolbar>
-              <Typography variant="body1" color="inherit">
-                © 2021 Dynamic Store từ từ làm tiếp nha
-              </Typography>
+              <Grid container>
+                <Grid item xs={6} xl={6}>
+                  <Typography variant="body1" color="inherit">
+                    © 2021 Dynamic Store từ từ làm tiếp nha
+                  </Typography>
+                </Grid>
+                <Grid item xs={6} xl={6}>
+                  <Typography variant="body1" color="inherit">
+                    Social
+                  </Typography>
+                </Grid>
+              </Grid>
             </Toolbar>
           </Container>
         </AppBar>
-    )
+      </>
+    );
+  } else return (
+    <>
+      is Mobile Footer
+    </>
+  )
 }
--- src/components/Typographys/OrdersCaseTypography.js
+++ src/components/Typographys/OrdersCaseTypography.js
@@ -2,14 +2,15 @@ import * as React from 'react';
 import { styled } from '@mui/material/styles';
 import { SystemColor } from '../../color';
 
-const Div = styled('div')(({ theme }) => ({
-  ...theme.typography.button,
-  backgroundColor: "transparent",
-  padding: theme.spacing(1),
-  color: SystemColor.main,
-  fontSize: "16px",
-}));
 
-export default function OrdersCaseTypography({text}) {
+
+export default function OrdersCaseTypography({text, size, color}) {
+  const Div = styled('div')(({ theme }) => ({
+    ...theme.typography.button,
+    backgroundColor: "transparent",
+    padding: theme.spacing(1),
+    color: SystemColor.main,
+    fontSize: "16px",
+  }));
   return <Div>{text}</Div>;
 }
