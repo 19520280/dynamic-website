477fd6f7c155971d55da19ba920c7b837c7a836a
yccyennhi <ycc.yennhi@gmail.com>
2021-12-17 18:07:15 +0700

    Update OrderStatus.js
    Update state of button

--- src/components/OrderStatus/OrderStatus.js
+++ src/components/OrderStatus/OrderStatus.js
@@ -1,10 +1,17 @@
 import { Button, Grid, Stack } from "@mui/material";
-import React from "react";
+import React, {useState} from "react";
 import OrdersCaseTypography from "../Typographys/OrdersCaseTypography";
 import PaymentCartLeftTable from "../Tables/PaymentCartLeftTable";
 import CountTypography from "../Typographys/CountTypography";
+import { useHistory } from "react-router-dom";
 
 export default function OrderStatus({ status }) {
+  const history = useHistory();
+  const [Status, setStatus]=useState(false);
+  const onClick =()=>{
+    setStatus(true);
+    console.log(Status);
+  }
   return (
     <Grid sx={{ paddingTop: "12px" }}>
       <Grid item container justifyContent="flex-end">
@@ -16,8 +23,8 @@ export default function OrderStatus({ status }) {
       </Grid>
       <Grid style={{paddingBottom:"12px"}} item container justifyContent="flex-end">
         <Stack direction="row" spacing={1}>
-          <Button variant="contained">LIÊN HỆ</Button>
-          <Button variant="outlined">HỦY ĐƠN</Button>
+          <Button disabled={Status} onClick={history.push("/Lien-he")} variant="contained">LIÊN HỆ</Button>
+          <Button disabled={Status} onClick={onClick} variant="outlined">HỦY ĐƠN</Button>
         </Stack>
       </Grid>
     </Grid>
