c9992c825604c134c7406c2ede5adebc881fff3f
Thao <19520280@gm.uit.edu.vn>
2021-12-20 00:01:15 +0700

    Fix MixMatchDialog

--- src/components/Dialogs/MixMatchDialog/MixMatchDialog.js
+++ src/components/Dialogs/MixMatchDialog/MixMatchDialog.js
@@ -6,6 +6,7 @@ import {
   Dialog,
   DialogContent,
   DialogTitle,
+  Divider,
   IconButton,
   Stack,
   Typography,
@@ -22,36 +23,6 @@ import ProductCard from "../../ProductCard/ProductCard";
 import React from "react";
 import { hideMixMatchDialog } from "../../../redux/actions";
 import { products } from "../../../dataSources/Products";
-
-const Actions = () => (
-  <div>
-    <Stack
-      direction="row"
-      justifyContent="stretch"
-      alignItems="flex-start"
-      spacing={2}
-    >
-      <Typography fontSize="1rem" fontWeight="bold" color="primary">
-        Số lượng
-      </Typography>
-      <Typography fontSize="1rem" fontWeight="bold" color="primary">
-        2 sản phẩm
-      </Typography>
-    </Stack>
-    <Stack
-      direction="row"
-      justifyContent="stretch"
-      alignItems="flex-start"
-      spacing={2}
-    >
-      <Typography fontSize="1rem" fontWeight="bold" color="primary">
-        Tổng cộng
-      </Typography>
-      <PriceTypography gia={370000} fontSize="1rem" />
-    </Stack>
-    <Button>Thêm tất cả vào giỏ hàng</Button>
-  </div>
-);
 
 const MixMatchDialog = ({ sanPham }) => {
   const theme = useTheme();
@@ -96,7 +67,12 @@ const MixMatchDialog = ({ sanPham }) => {
       scroll="paper"
       aria-labelledby="scroll-dialog-title"
       aria-describedby="scroll-dialog-description"
-      sx={{ maxHeight: "100%" }}
+      sx={{
+        maxHeight: "100%",
+        "& .css-10jb4jx-MuiPaper-root-MuiDialog-paper": {
+          maxWidth: "none",
+        },
+      }}
     >
       <DialogTitle id="scroll-dialog-title">
         Phối đồ
@@ -122,11 +98,15 @@ const MixMatchDialog = ({ sanPham }) => {
             <ImageHover imgs={[sanPham.imgs[0], sanPham.imgs[1]]} />{" "}
           </Box>
           <Stack direction="column" spacing={1} sx={{ width: "100%" }}>
-            <Stack direction="row" spacing={2}>
+            <Typography variant="button" color="secondary">
+              Sản phẩm đính kèm
+            </Typography>
+            <Stack direction="row" spacing={1}>
               {listProducts.map((product, index) => (
                 <ProductCard sanPham={product} key={index} />
               ))}
             </Stack>
+            <Divider />
             <Actions />
           </Stack>
         </Stack>
@@ -136,3 +116,34 @@ const MixMatchDialog = ({ sanPham }) => {
 };
 
 export default MixMatchDialog;
+
+const Actions = () => (
+  <Stack direction="row" justifyContent="space-between">
+    <div>
+      <Typography
+        fontSize="1rem"
+        fontWeight="bold"
+        color="primary"
+        display="inline"
+        marginRight={4}
+      >
+        Số lượng
+      </Typography>
+      <Typography
+        fontSize="1rem"
+        fontWeight="bold"
+        color="primary"
+        display="inline"
+      >
+        2 sản phẩm
+      </Typography>
+      <Stack direction="row" justifyContent="flex-start" spacing={3}>
+        <Typography fontSize="1rem" fontWeight="bold" color="primary">
+          Tổng cộng
+        </Typography>
+        <PriceTypography gia={370000} fontSize="1rem" />
+      </Stack>
+    </div>
+    <Button variant="contained">Thêm vào giỏ hàng</Button>
+  </Stack>
+);
